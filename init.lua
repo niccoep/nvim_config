ed7e14539d8c941b30c52d62fc3598e32ebc7882
niccoep <niccoep@gmail.com>
2025-09-13 19:51:17 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,71 @@
+local home = os.getenv( 'HOME' )
+
+vim.g.mapleader = " "
+
+--color
+vim.cmd("colo habamax")
+vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+--vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.updatetime = 300
+vim.opt.ignorecase = true
+vim.opt.winborder = "rounded"
+vim.opt.signcolumn = "yes"
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+
+vim.diagnostic.config({
+  float = {
+    border = "rounded"
+  }
+})
+
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>s', ':source<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+--vim.keymap.set('n', '<leader>', '')
+
+vim.lsp.config( 'luals', {
+    cmd = { home .. '/.local/lua-language-server/bin/lua-language-server' },
+    root_markers = { '.git' },
+    filetypes = { 'lua' },
+    settings = {
+        lua = {
+            diagnostics = {
+                globals = { 'vim' },
+            },
+            runtime = {
+                version =  'LuaJIT'
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("",true),
+            },
+        }
+    }
+})
+
+--vim.lsp.config( 
+
+vim.lsp.enable( 'luals' )
+
+vim.keymap.set( 'n', 'gd', vim.lsp.buf.definition )
+vim.keymap.set( 'n', 'gD', vim.lsp.buf.declaration )
+vim.keymap.set( 'n', 'gr', vim.lsp.buf.references )
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
+
+
+
